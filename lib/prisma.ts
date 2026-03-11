5474ecdaf3c1818f6f5a0591531a3896b9513835
Ricardo Elito <ricardo.elito@gmail.com>
2026-03-10 21:24:33 -0300

    banco_dados

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import path from 'path'
-
-const dbUrl = process.env.DATABASE_URL ?? `file:${path.resolve('./dev.db')}`
-const adapter = new PrismaBetterSqlite3({ url: dbUrl })
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
