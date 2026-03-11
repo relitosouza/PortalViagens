bbe6d398431c307f39075d4432ab9f859a4d3bd2
Ricardo Elito <ricardo.elito@gmail.com>
2026-03-10 21:44:21 -0300

    atrualização bd

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,12 @@
-import "dotenv/config";
+import path from "path";
+import * as dotenv from "dotenv";
 import { defineConfig } from "prisma/config";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env["DATABASE_URL"] || "file:./dev.db",
+    url: process.env.DATABASE_URL,
   },
 });
